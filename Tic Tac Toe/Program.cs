974d1b698fa255d4fcfbd7d0b7fb92332be9ebe4
Frontabl3 <frontabl3@gmail.com>
2024-03-07 01:15:53 +0200

    Adding logic to find it the current player has won

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -4,9 +4,17 @@ namespace Tic_Tac_Toe
     class Program
     {
         static string[] board = new string[9];
+        static void initializeBoard()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = i.ToString();
+            }
+        }
         static void Main(string[] args)
         {
-            string name = "X";
+            //string name = "X";
+            initializeBoard();  
             start();
         }
 
@@ -36,7 +44,8 @@ namespace Tic_Tac_Toe
 
         static void askPlayer(string player)
         {
-            Console.WriteLine("Player " + player + " input: ");
+            Console.WriteLine("Player " + player + " plaes enter a number from 0 to 8: ");
+
             int select = Convert.ToInt32(Console.ReadLine());
             board[select] = player;
             drawBoard();
@@ -44,7 +53,21 @@ namespace Tic_Tac_Toe
 
         static Boolean winCon()
         {
+
+            if (String.Equals(board[0], board[1]) && String.Equals(board[1], board[2]))
+            {
+                Console.WriteLine("GG");
+                return true;
+            }    
+         
+            if (String.Equals(board[3], board[4]) && String.Equals(board[4], board[5]))
+            {
+                Console.WriteLine("GG");
+                return true;
+            }
+
             return false;
+                
         }
 
     }
