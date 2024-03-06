99be590f983076146c90c8a4ae483382be393f7b
Frontabl3 <frontabl3@gmail.com>
2024-03-07 00:42:54 +0200

    Base of game

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static int[] board = new int[9];
+        static void Main(string[] args)
+        {
+            string name = "X";
+            start();
+        }
+
+
+        static void start()
+        {
+            Console.Title = "Tic Tac Toe";
+            Console.WriteLine("Hello.");
+            drawBoard();
+            while(winCon() == false)
+            {
+                askPlayer("X");
+                if (winCon() == true)
+                    break;
+                askPlayer("O");
+            }
+        }
+
+        static void drawBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("| " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + " |");
+            }
+            
+        }
+
+        static void askPlayer(string player)
+        {
+            Console.WriteLine("Enter Input: ");
+            int select = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0;i < board; i++) { }
+        }
+
+        static Boolean winCon()
+        {
+            return false;
+        }
+
+    }
+
+}
